cf25333acfefb95c44952ced736bdea684964c98
Benjamin Nhan <benjaminnhan12@gmail.com>
2025-07-29 09:29:09 -0700

    testing

--- src/app/api/register/route.ts
+++ src/app/api/register/route.ts
@@ -1,7 +1,7 @@
 import { NextResponse } from 'next/server';
 import bcrypt from 'bcryptjs';
 import prisma from '@/lib/prisma';
-
+//register
 export async function POST(req: Request) {
   try {
     const { email, password, name } = await req.json();
