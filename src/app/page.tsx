2bd062f7bdd373a4e433524c543c2816b58e946b
Ryan <ryan.lee5353@gmail.com>
2025-08-05 19:30:06 -0400

    changed so default page redirects to login

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,25 +2,12 @@ import { getServerSession } from 'next-auth';
 import { authOptions } from '@/app/api/auth/[...nextauth]/route';
 import { redirect } from 'next/navigation';
 import LogoutButton from '@/components/LogoutButton';
-import Link from "next/link";
 
 export default async function AdminPage() {
   const session = await getServerSession(authOptions);
 
   if (!session) {
-    return (
-      <div className="p-4">
-        <h1 className="text-xl font-semibold text-red-600">Unauthorized</h1>
-        <p className="text-gray-600 mt-2">You must be signed in to view this page.</p>
-
-        <Link
-          href="/login"
-          className="inline-block mt-4 bg-blue-600 text-white px-4 py-2 rounded hover:bg-blue-700 transition"
-        >
-          Go to Login
-        </Link>
-      </div>
-    );
+    redirect("/login");
   }
 
   const role = session.user.role;
