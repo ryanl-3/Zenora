dec2bc438e7aedb803aed18af93ea05cdbfe04e0
Ryan <ryan.lee5353@gmail.com>
2025-08-11 22:34:18 -0400

    edited module for nextauth route for added user roles

--- src/app/api/auth/[...nextauth]/d.ts
+++ src/app/api/auth/[...nextauth]/d.ts
@@ -1,23 +1,23 @@
-import NextAuth from "next-auth";
+import { DefaultSession } from "next-auth";
 
 declare module "next-auth" {
   interface Session {
     user: {
       id: string;
-      email: string;
-      name?: string;
-    };
+      role: "ADMIN" | "USER";
+    } & DefaultSession["user"];
   }
 
   interface User {
     id: string;
-    email: string;
-    name?: string;
+    role: "ADMIN" | "USER";
   }
 }
 
 declare module "next-auth/jwt" {
   interface JWT {
-    id?: string;
+    id: string;
+    role: "ADMIN" | "USER";
   }
 }
+
